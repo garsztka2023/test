6eae7b838f793ba662c8cb696f36931e1c27076a
garsztka2023 <148491790+garsztka2023@users.noreply.github.com>
2023-10-19 21:17:41 +0200

    Add files via upload

---
+++ t19-cw3a.cpp
@@ -0,0 +1,40 @@
+/*
+Uruchom powyzszy program i sprawdz jego dzialanie.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+const int N = 4;
+const int NOMINALY[] = {1,2,4,5};
+const int KWOTA = 8;
+
+int IleMonet()
+{
+    int i, j;
+    int IleM[KWOTA + 1];
+    IleM[0] = 0;
+
+    for(i = 1; i <= KWOTA; i++)
+        IleM[i] = KWOTA + 1;
+
+    for(i = 1; i <= KWOTA; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            if(NOMINALY[j] <= i)
+                if(IleM[i - NOMINALY[j]] + 1 < IleM[i])
+                    IleM[i] = IleM[i - NOMINALY[j]] + 1;
+        }
+    }
+
+    return IleM[KWOTA];
+}
+
+int main()
+{
+    cout << "Minimalna liczba monet dla kwoty ";
+    cout << KWOTA << ": " << IleMonet();
+    return 0;
+}
